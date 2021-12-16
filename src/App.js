90144379d35bfd9593b65a6310fdd387a95561bd
boikopetro <boikopetromail@gmail.com>
2021-12-16 11:11:58 +0200

    add sortPosts function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import './styles/App.css'
 import {useState} from "react";
 import PostsList from "./components/PostsList";
 import PostForm from "./components/UI/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'aaa', body: 'sss'},
+        {id: 2, title: 'dddd', body: 'ggg'},
+        {id: 3, title: 'rrr', body: 'bbb'}
+    ])
+
+    const [selectedSort, setSelectedSort] = useState('')
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -14,9 +21,27 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
+
     return (
         <div>
             <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue='name'
+                    options={[
+                        {value: 'title', name: 'name'},
+                        {value: 'body', name: 'description'},
+
+                    ]}
+                />
+            </div>
             {
                 posts.length
                     ? <PostsList remove={removePost} posts={posts} title='List 1'/>
