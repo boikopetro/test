8e6f4625f6847bda2afc9fd99c1fa5b93e7341cf
boikopetro <boikopetromail@gmail.com>
2021-12-16 13:35:30 +0200

    add animation to creating/deleting posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,19 @@
 import Post from "./Post";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostsList = ({posts, title, remove}) => {
     return (
         <div className="App">
             <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {
-                posts.map((p, i) =>
-                    <Post remove={remove} key={p.id} post={p} number={i + 1}/>)
-            }
+            <TransitionGroup>
+                {
+                    posts.map((p, i) =>
+                        <CSSTransition key={p.id} timeout={500} classNames={'post'}>
+                            <Post remove={remove} post={p} number={i + 1}/>
+                        </CSSTransition>
+                    )
+                }
+            </TransitionGroup>
         </div>
     );
 };
