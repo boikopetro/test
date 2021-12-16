43176d49752a59d9a057c1d10b35a5b04cf7b140
boikopetro <boikopetromail@gmail.com>
2021-12-16 17:47:39 +0200

    add loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './styles/App.css'
-import {useEffect, useMemo, useState} from "react";
+import {useEffect, useState} from "react";
 import PostsList from "./components/PostsList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
     useEffect(fetchPosts, [])
@@ -14,6 +15,7 @@ function App() {
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [isPostLoading, setIsPostLoading] = useState(false)
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -25,8 +27,10 @@ function App() {
     }
 
     async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPosts(response.data)
+        setIsPostLoading(true)
+        const posts = await PostService.getAll()
+        setPosts(posts)
+        setIsPostLoading(false)
     }
 
     return (
@@ -38,7 +42,10 @@ function App() {
 
             <hr style={{margin: '15px 0'}}/>
             <PostFilter posts={posts} filter={filter} setFilter={setFilter}/>
-            <PostsList remove={removePost} posts={sortedAndSearchedPosts} title='List 1'/>
+            {isPostLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+                : <PostsList remove={removePost} posts={sortedAndSearchedPosts} title='List 1'/>
+            }
         </div>
     );
 }
