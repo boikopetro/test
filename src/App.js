aca19bfd3c28372b36b1a9e800774d3c54ab4c77
boikopetro <boikopetromail@gmail.com>
2021-12-16 14:02:18 +0200

    add posts request to jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import './styles/App.css'
-import {useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import PostsList from "./components/PostsList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'aaa', body: 'sss'},
-        {id: 2, title: 'dddd', body: 'ggg'},
-        {id: 3, title: 'rrr', body: 'bbb'}
-    ])
-
+    useEffect(fetchPosts, [])
+    const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-
-    const sortedPosts = useMemo(() => {
-        console.log('aa')
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts
-    }, [filter.sort, posts])
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(p => p.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts])
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -35,6 +22,11 @@ function App() {
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
     }
 
     return (
